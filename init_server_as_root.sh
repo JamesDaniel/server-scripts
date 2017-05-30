c3917786346fb4c48be4a904c17f5ad1c4fed96e
James <james.d.mcgarr@students.ittralee.ie>
2017-05-30 05:50:26 +0100

    Update init_server_as_root.sh
    Added comments and droplet monitoring.

--- init_server_as_root.sh
+++ init_server_as_root.sh
@@ -1,10 +1,13 @@
+# Create new user with sudo priviledges.
 sudo adduser user
 sudo adduser user sudo
 sudo gpasswd -a user sudo
+
+# Setup ssh key
 mkdir /home/user/.ssh
 chown user:user /home/user/.ssh
 cp ~/.ssh/authorized_keys /home/user/.ssh/authorized_keys
 chown user:user /home/user/.ssh/authorized_keys
 
-#Suggested next script:
-# init_server_as_user.sh
+# Update droplet monitoring
+curl -sSL https://agent.digitalocean.com/install.sh | sh
